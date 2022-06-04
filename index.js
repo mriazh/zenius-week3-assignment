e7c663494a5e249ff2195ccb6bc9d629ee315ef9
mriazh <192410102059@mail.unej.ac.id>
2022-06-04 16:11:43 +0700

    first

--- index.js
+++
@@ -1,6 +0,0 @@
-// ReactDOM.render(<h1 className="header">Hello, React!</h1>, document.getElementById("root"))
-
-// const h1 = document.createElement("h1")
-// h1.textContent = "This is an imperative way to program"
-// h1.className = "header"
-// document.getElementById("root").append(h1)
